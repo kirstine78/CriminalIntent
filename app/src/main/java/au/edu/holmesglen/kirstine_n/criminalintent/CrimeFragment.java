cfa166522e0e8252674cee4a6515b57d6c086447
kirsti <kirstine78@gmail.com>
2017-03-06 12:14:58 +1100

    basic done chapter 7

---
+++ app/src/main/java/au/edu/holmesglen/kirstine_n/criminalintent/CrimeFragment.java
@@ -0,0 +1,55 @@
+package au.edu.holmesglen.kirstine_n.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+/**
+ * Created by Kirsti on 6/03/2017.
+ */
+
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+    private EditText mTitleField;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    // inflate fragment_crime.xml
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // 3rd param tells the layout inflater whether to add the inflated view to the view's parent
+        // we pass in false because we will add the view in the activity's code
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        return v;
+    }
+}
