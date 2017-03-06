a7bb6532002e7cdf697fc58fbc3cd3e3d667274b
kirsti <kirstine78@gmail.com>
2017-03-06 13:15:13 +1100

    complete chapter 8, before challenge

--- app/src/main/java/au/edu/holmesglen/kirstine_n/criminalintent/Crime.java
+++ app/src/main/java/au/edu/holmesglen/kirstine_n/criminalintent/Crime.java
@@ -1,6 +1,8 @@
 package au.edu.holmesglen.kirstine_n.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
+
 
 /**
  * Created by Kirsti on 6/03/2017.
@@ -9,10 +11,13 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime() {
         // generate unique identifier
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId() {
@@ -25,5 +30,21 @@ public class Crime {
 
     public void setTitle(String title) {
         mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 }
