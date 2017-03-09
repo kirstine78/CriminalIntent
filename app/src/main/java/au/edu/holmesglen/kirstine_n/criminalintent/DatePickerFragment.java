5813944de9e9884f59df2340688f8155bf892065
kirsti <kirstine78@gmail.com>
2017-03-09 13:27:44 +1100

    add DatePickerFragment class

---
+++ app/src/main/java/au/edu/holmesglen/kirstine_n/criminalintent/DatePickerFragment.java
@@ -0,0 +1,21 @@
+package au.edu.holmesglen.kirstine_n.criminalintent;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+/**
+ * Created by Kirsti on 9/03/2017.
+ */
+
+public class DatePickerFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    } // end onCreateDialog
+}  // end class
